113bdeaec108336be04fb9ca810b5906ab156596
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2022-11-19 13:04:51 -0800

    initcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AdminPage from './pages/AdminPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column min-vh-100">
+        <header>
+          <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container>
+              <Navbar.Brand>HelpDesk</Navbar.Brand>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className="me-auto w-100 justify-content-end">
+                  <a href='/admin' className='nav-link' target='_blank'>
+                    Admin
+                  </a>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className='mt-3'>
+            <Routes>
+              <Route path='/admin' element={<AdminPage />}></Route>
+              <Route path='/' element={<HomePage />}></Route>
+            </Routes>
+          </Container> 
+        </main>
+        <footer className='mt-auto'>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
